22a71d64fb8f2936703034f9c00a583bc4aa08ff
Austyn Gerzevske <agerzevske@hawk.iit.edu>
2018-12-06 15:27:10 -0800

    Add js for menu menu button
      Menu button working for all three pages
      successfully re/disappears

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,25 @@
-// Idea use js to skip decenders for not chrome
+//<h2 class="nav-h2"><a href="#nav-site">Menu<a/></h2>
+
+var html = document.querySelector('html');
+html.classList.remove('nojs');
+html.classList.add('js');
+
+var navigation = document.querySelector('.nav-site');
+var nav_list = document.querySelector('.nav-menu')
+
+var nav_heading = document.createElement('h2');
+
+var menu_link = document.createElement('a');
+menu_link.textContent = 'Menu';
+menu_link.setAttribute('id','menu-button');
+menu_link.setAttribute('href','#null');
+
+nav_heading.append(menu_link);
+
+navigation.insertBefore(nav_heading,nav_list);
+
+var menu_button = document.querySelector('#menu-button');
+menu_button.addEventListener('click', function(e) {
+  nav_list.classList.toggle('visible');
+  e.preventDefault();
+})
